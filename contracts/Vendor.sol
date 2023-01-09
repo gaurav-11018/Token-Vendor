3d1564529c8e423cc5d374db103a518febde7be8
Gaurav <79459872+gaurav-11018@users.noreply.github.com>
2023-01-09 23:03:50 +0530

    Add files via upload

---
+++ contracts/Vendor.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./YourToken.sol";
+
+contract Vendor is Ownable {
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+
+    YourToken public yourToken;
+    uint256 public constant tokensPerEth = 100;
+
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
+
+    function buyTokens() public payable {
+        uint256 tokensToBuy = msg.value * tokensPerEth;
+
+        // Transfer the tokens to the buyer
+        yourToken.transfer(msg.sender, tokensToBuy);
+
+        // Emit the BuyTokens event
+        emit BuyTokens(msg.sender, msg.value, tokensToBuy);
+    }
+
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+
+    // ToDo: create a sellTokens(uint256 _amount) function:
+}
