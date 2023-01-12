0e3e5bec9dbb5a0fe33c2a57d6ca4b2f68ca0e44
Gaurav <79459872+gaurav-11018@users.noreply.github.com>
2023-01-12 23:14:19 +0530

    Update Vendor.sol

--- contracts/Vendor.sol
+++ contracts/Vendor.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
-
+pragma solidity 0.8.4; 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
