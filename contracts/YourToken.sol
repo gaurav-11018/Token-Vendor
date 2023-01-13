ab97e61889bf94f89cdae90aa3c6119cf4e80287
Gaurav <79459872+gaurav-11018@users.noreply.github.com>
2023-01-13 23:19:52 +0530

    Update YourToken.sol

--- contracts/YourToken.sol
+++ contracts/YourToken.sol
@@ -6,5 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract YourToken is ERC20 {
     constructor() ERC20("Basic", "BIC") {
         _mint(msg.sender, 1000 * 10 ** 18);
+    
     }
 }
